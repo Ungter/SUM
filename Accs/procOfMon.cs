14457ed8a20706fc27abc0220881c8c94ee8e135
Ungter <59937326+Ungter@users.noreply.github.com>
2021-09-08 21:41:48 -0400

    Add files via upload

---
+++ Accs/procOfMon.cs
@@ -0,0 +1,66 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+
+namespace sum {
+    public class procOfMon : ModPlayer {
+
+
+        public bool FrostBurnSummon;
+
+        public override void ResetEffects() {
+            FrostBurnSummon = false;
+        }
+
+        
+        public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit) {
+            if ((proj.minion || ProjectileID.Sets.MinionShot[proj.type]) && FrostBurnSummon && !proj.noEnchantments) {
+
+                target.AddBuff(BuffID.Frostburn, 60 * Main.rand.Next(5, 15), false);
+            }
+        }
+
+    }
+   
+    // We nest the acc inside the mod namespace to attach the defined effect above to the accs
+    namespace Accs.AccSprites {
+        
+        // Loads the png or animations
+        [AutoloadEquip(EquipType.Neck)]
+
+        internal class procOfMonarch : ModItem {
+
+            // Sets the Name and Tooltip for the accs
+            public override void SetStaticDefaults() {
+                DisplayName.SetDefault("Shiny Blue Necklace");
+                Tooltip.SetDefault("You found(made ?) this shiny necklace inside an igloo");
+            }
+
+            // Sets the stats for the accs
+            public override void SetDefaults() {
+                item.width = 35;
+                item.height = 35;
+                item.rare = ItemRarityID.Yellow;
+                item.accessory = true;
+                item.value = 150000;
+            }
+
+            // Since the player itself can't summon the frostburn effect because Player isn't attached to the effect, we need to use the GetModPlayer method to retrieve ModPlayer instance attached to the specified Player.
+            public override void UpdateAccessory(Player player, bool hideVisual) {
+                player.GetModPlayer<procOfMon>().FrostBurnSummon = true;
+            }
+
+            // Adds the recipe to make the actual accs
+            public override void AddRecipes() {
+
+                ModRecipe recipie = new ModRecipe(mod);
+                // Remember to actually change this recipie pls
+                recipie.AddIngredient(ItemID.DirtBlock, 1);
+                recipie.AddTile(TileID.WorkBenches);
+                recipie.SetResult(this);
+                recipie.AddRecipe();
+
+            }
+        }
+    }
+}
---
+++ Buffs/stronk.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace sum.Buffs {
+
+	public class stronk : ModBuff {
+
+        public override void SetDefaults() { 
+
+            DisplayName.SetDefault("Stronk");
+            Description.SetDefault("Makes you strong burh");
+            canBeCleared = false;
+                            
+        }
+
+        public override void Update(Player player, ref int buffIndex) { 
+        
+            player.lifeRegen += 200;
+            player.extraAccessorySlots += 2;
+            player.moveSpeed += 9999;
+            if (player.lifeRegen <= 200) { 
+            
+                player.lifeRegen++;
+            }
+
+        }
+
+        
+    }
+}
